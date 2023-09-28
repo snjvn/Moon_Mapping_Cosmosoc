d185ab4ed2d586ef5c5179d9224de11548e606d3
snjvn <91363279+snjvn@users.noreply.github.com>
2023-09-28 23:49:30 +0530

    Add files via upload
    most recent

--- Network.py
+++ Network.py
@@ -25,7 +25,7 @@ from tensorflow.keras.layers import Conv2D, Conv2DTranspose
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import LeakyReLU, PReLU
 from tensorflow.keras.layers import add
-
+from skimage.transform import rescale, resize
 
 # Residual block
 def res_block_gen(model, kernal_size, filters, strides):
@@ -48,11 +48,14 @@ def up_sampling_block(model, kernal_size, filters, strides):
     
     # In place of Conv2D and UpSampling2D we can also use Conv2DTranspose (Both are used for Deconvolution)
     # Even we can have our own function for deconvolution (i.e one made in Utils.py)
-    #model = Conv2DTranspose(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
-    model = UpSampling2D(size = 2)(model)
+    print(model.shape,'2')
+    model = Conv2DTranspose(filters = filters, kernel_size = kernal_size, strides = strides, padding = "valid")(model)
+    # model = Conv2D(filters = filters, kernel_size = kernal_size, strides = strides, padding = "same")(model)
+    print(model.shape, "1")
+    # model = UpSampling2D(size = 2)(model)
+    # print(model.shape,'2')
     model = LeakyReLU(alpha = 0.2)(model)
-    
+
     return model
 
 
@@ -73,33 +76,42 @@ class Generator(object):
 
     def generator(self):
         
-	    gen_input = Input(shape = self.noise_shape)
-	    
-	    model = Conv2D(filters = 64, kernel_size = 9, strides = 1, padding = "same")(gen_input)
-	    model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
-	    
-	    gen_model = model
-        
-        # Using 16 Residual Blocks
-	    for index in range(16):
-	        model = res_block_gen(model, 3, 64, 1)
-	    
-	    model = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(model)
-	    model = BatchNormalization(momentum = 0.5)(model)
-	    model = add([gen_model, model])
-	    
-	    # Using 2 UpSampling Blocks
-	    for index in range(2):
-	        model = up_sampling_block(model, 3, 256, 1)
-	    
-	    model = Conv2D(filters = 3, kernel_size = 9, strides = 1, padding = "same")(model)
-	    model = Activation('tanh')(model)
-	   
-	    generator_model = Model(inputs = gen_input, outputs = model)
-        
-	    return generator_model
+        gen_input = Input(shape = self.noise_shape)
 
-# Network Architecture is same as given in Paper https://arxiv.org/pdf/1609.04802.pdf
+        model = Conv2D(filters = 64, kernel_size = 9, strides = 1, padding = "same")(gen_input)
+        model = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(model)
+
+        gen_model = model
+
+        # Using 16 Residual Blocks
+        for index in range(16):
+            model = res_block_gen(model, 3, 64, 1)
+        # print(model.shape)
+        model = Conv2D(filters = 64, kernel_size = 3, strides = 1, padding = "same")(model)
+        model = BatchNormalization(momentum = 0.5)(model)
+        model = add([gen_model, model])
+        # print(model.shape)
+
+        # Using 2 UpSampling Blocks
+        for index in range(2):
+            model = up_sampling_block(model, 3, 256, 5)
+        
+        # print(model.shape)
+
+        model = Conv2D(filters = 1, kernel_size = 9, strides = 1, padding = "same")(model)
+
+        # print(model.shape)
+
+        model = Activation('tanh')(model)
+        # model = resize(model,(1200,9369))   
+        # print(model.shape)  
+        #model = Flatten()(model)
+
+        generator_model = Model(inputs = gen_input, outputs = model)
+
+        return generator_model
+
+# Network Architecture is same as given in Paper https://arxiv.org/pdf/1609.04802.pdf 260, 1476
 class Discriminator(object):
 
     def __init__(self, image_shape):
@@ -122,8 +134,8 @@ class Discriminator(object):
         model = discriminator_block(model, 512, 3, 2)
         
         model = Flatten()(model)
-        model = Dense(512)(model)
-        model = LeakyReLU(alpha = 0.2)(model)
+        # model = Dense(64)(model)
+        # model = LeakyReLU(alpha = 0.2)(model)
        
         model = Dense(1)(model)
         model = Activation('sigmoid')(model) 
--- config.py
+++ config.py
@@ -18,13 +18,13 @@ config.TRAIN.lr_decay = 0.1
 config.TRAIN.decay_every = int(config.TRAIN.n_epoch / 2)
 
 ## train set location
-config.TRAIN.hr_img_path = '/media/ubuntu/disk/SRGAN-master/ohrc_final_final/y_train'
-config.TRAIN.lr_img_path = '/media/ubuntu/disk/SRGAN-master/resized_cropped_tmc/X_train'
+config.TRAIN.hr_img_path = '/media/ubuntu/disk/SRGAN-master/ohrc_final_final/y_train/patches'
+config.TRAIN.lr_img_path = '/media/ubuntu/disk/SRGAN-master/resized_cropped_tmc/X_train/patches'
 
 config.VALID = edict()
 ## test set location
-config.VALID.hr_img_path = '/media/ubuntu/disk/SRGAN-master/ohrc_final_final/y_test'
-config.VALID.lr_img_path = '/media/ubuntu/disk/SRGAN-master/resized_cropped_tmc/X_test'
+config.VALID.hr_img_path = '/media/ubuntu/disk/SRGAN-master/ohrc_final_final/y_test/patches'
+config.VALID.lr_img_path = '/media/ubuntu/disk/SRGAN-master/resized_cropped_tmc/X_test/patches'
 
 def log_config(filename, cfg):
     with open(filename, 'w') as f:
